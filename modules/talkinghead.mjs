1336f22eb6c4d60e993c961a9629c4caa2d9caa2
DesarguesC <425593160@qq.com>
2025-10-09 22:54:22 -0400

    lookat return

--- modules/talkinghead.mjs
+++ modules/talkinghead.mjs
@@ -3977,7 +3977,7 @@ class TalkingHead {
   * @param {number} t Time in milliseconds
   */
   lookAt(x,y,t) {
-    // return;
+    return;
     // Eyes position
     const rect = this.nodeAvatar.getBoundingClientRect();
     this.objectLeftEye.updateMatrixWorld(true);
