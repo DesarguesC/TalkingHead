e3a6b38862bdba62c1b9c33c677fe623d87d3462
DesarguesC <425593160@qq.com>
2025-11-17 01:07:03 -0500

    hide status window

--- modules/talkinghead.mjs
+++ modules/talkinghead.mjs
@@ -199,13 +199,14 @@ class TalkingHead {
     Object.assign( this.opt, opt || {} );
 
     // Statistics
-    if ( this.opt.statsNode ) {
-      this.stats = new Stats();
-      if ( this.opt.statsStyle ) {
-        this.stats.dom.style.cssText = this.opt.statsStyle;
-      }
-      this.opt.statsNode.appendChild( this.stats.dom );
-    }
+    // if ( this.opt.statsNode ) {
+    //   this.stats = new Stats();
+    //   if ( this.opt.statsStyle ) {
+    //     this.stats.dom.style.cssText = this.opt.statsStyle;
+    //   }
+    //   this.opt.statsNode.appendChild( this.stats.dom );
+    // }
+    // Hide the status window
 
     this.ChineseNumber = {'0': '零','1': '一','2': '二','3': '三','4': '四','5': '五','6': '六','7': '七','8': '八','9': '九'}
     this.ChineseUnit = ['十','百','千','万','亿']
